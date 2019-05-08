96eab347b11aa2bfbbe4e5aedc68e4b3fb24f52d
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-08 17:07:24 -0400

    Imported the users router. Created base url for users

--- api/server.js
+++ api/server.js
@@ -4,9 +4,13 @@ const morgan = require('morgan');
 
 const server = express();
 
+const usersRouter = require('../users/users-router');
+
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
+
+server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
     res.send('Connected!');
