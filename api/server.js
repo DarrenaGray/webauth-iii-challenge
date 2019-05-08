a41c7dbb9af01fd35076ac0c201fc68b479022e0
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-08 17:51:44 -0400

    Imported auth routers and created base url for auth routes.

--- api/server.js
+++ api/server.js
@@ -5,12 +5,14 @@ const morgan = require('morgan');
 const server = express();
 
 const usersRouter = require('../users/users-router');
+const authRouter = require('../auth/auth-router');
 
 server.use(express.json());
 server.use(helmet());
 server.use(morgan('dev'));
 
 server.use('/api/users', usersRouter);
+server.use('/api/auth', authRouter);
 
 server.get('/', (req, res) => {
     res.send('Connected!');
