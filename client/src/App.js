dca68e3459f0c801563604af39186e32c30364f0
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-09 19:48:53 -0400

    Imported SignUp component. Added NavLinks for signup and login

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
 import React from 'react';
 import { Route, NavLink } from 'react-router-dom';
 
+import SignUp from './auth/SignUp';
 import Login from './auth/Login';
 import UsersList from './components/UsersList';
 
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <div>
-        <NavLink to='/'>Users</NavLink>
-      </div>
-      <Route path='/' component={UsersList} />
-      <Route path='/' component={Login} />
-
-    </div>
-  );
+	return (
+		<div className='App'>
+			<div>
+				<NavLink to='/users'>Users</NavLink>
+				<NavLink to='/signup'>SignUp</NavLink>
+				<NavLink to='/login'>Login</NavLink>
+			</div>
+			<Route exact path='/users' component={UsersList} />
+			<Route path='/signup' component={SignUp} />
+			<Route path='/login' component={Login} />
+		</div>
+	);
 }
 
 export default App;
