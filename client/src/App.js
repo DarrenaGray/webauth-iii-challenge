5134e865e51596065c674e8706da92001bf2120b
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-09 16:46:55 -0400

    Imported UsersList component

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Route, NavLink } from 'react-router-dom';
+
+import UsersList from './components/UsersList';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <NavLink to='/'>Users</NavLink>
+      </div>
+      <Route path='/' component={UsersList} />
 
     </div>
   );
