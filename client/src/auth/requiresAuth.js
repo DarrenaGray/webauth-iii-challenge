2d596738eeb3a5729055668069382bdb928b3ee6
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-09 18:36:56 -0400

    Created requireAuth HOC for handling localStorage.getItem() when retrieving the jwt

---
+++ client/src/auth/requiresAuth.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function(Component) {
+	return class Authenticated extends React.Component {
+		render() {
+			const token = localStorage.getItem('jwt');
+            const notLoggedIn = <h3>Please login to see the users</h3>;
+            
+			return (token ? <Component {...this.props} /> : notLoggedIn);
+		}
+	};
+}
