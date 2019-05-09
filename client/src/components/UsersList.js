2dceac9bc04d7462be35503d7b252207b7519f21
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-09 19:52:27 -0400

    Format updates

--- client/src/components/UsersList.js
+++ client/src/components/UsersList.js
@@ -1,43 +1,42 @@
 import React from 'react';
 import axios from 'axios';
 
+import requiresAuth from '../auth/requiresAuth';
+
 class UsersList extends React.Component {
-    state = {
-        users: []
-    }
+	state = {
+		users: []
+	};
 
-    componentDidMount() {
-        console.log('Mounting users...')
-        const baseUrl = 'http://localhost:4000/api'
-        axios
-            .get(`${baseUrl}/users`)
-            .then(res => {
-                console.log(res)
-                this.setState({
-                    users: res.data
-                })
-            })
-            .catch(err => {
-                console.log(err)
-            })
-    }
+	componentDidMount() {
+		console.log('Mounting users...');
+		const baseUrl = 'http://localhost:4000/api';
+		axios
+			.get(`${baseUrl}/users`)
+			.then(res => {
+				console.log(res);
+				this.setState({
+					users: res.data
+				});
+			})
+			.catch(err => {
+				console.log(err);
+			});
+	}
 
-
-    render() {
-        console.log('Rendering users...')
-        return (
-            <div>
-                <h3>Users</h3>
-                {this.state.users.map(user => (
-                    <div key={user.id}>
-                        <p>{user.username}</p>
-                    </div>
-                ))}
-
-            </div>
-        )
-    }
+	render() {
+		console.log('Rendering users...');
+		return (
+			<div className='usersList'>
+				<h3>Users</h3>
+				{this.state.users.map(user => (
+					<div className='userInfo' key={user.id}>
+						<p>{user.username}</p>
+					</div>
+				))}
+			</div>
+		);
+	}
 }
 
-
-export default UsersList;
+export default requiresAuth(UsersList);
