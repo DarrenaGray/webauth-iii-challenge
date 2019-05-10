84eafdb343ae6bb9e1e6e56a147a2e7b999c13ee
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-10 00:41:49 -0400

    Added default base url to be used universally throughout app. Added axios.interceptors.request.use() that allows axios to deal with getting authorization token headers for all components that need them

--- client/src/auth/requiresAuth.js
+++ client/src/auth/requiresAuth.js
@@ -1,4 +1,19 @@
 import React from 'react';
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:4000/api'; // process.env.REACT_APP_API_URL
+
+// Allows axios to deal with getting authorization token headers for all components that need them
+axios.interceptors.request.use(
+	function(requestConfig) {
+		requestConfig.headers.authorization = localStorage.getItem('jwt');
+
+		return requestConfig;
+	},
+	function(error) {
+		return Promise.reject(error);
+	}
+);
 
 export default function(Component) {
 	return class Authenticated extends React.Component {
