d8767a32e10dea17f87b5a84ddfcc791f31d517c
Darrena Gray <40064931+WhatTheSpork@users.noreply.github.com>
2019-05-10 00:01:27 -0400

    Formatted

--- client/src/auth/requiresAuth.js
+++ client/src/auth/requiresAuth.js
@@ -4,9 +4,9 @@ export default function(Component) {
 	return class Authenticated extends React.Component {
 		render() {
 			const token = localStorage.getItem('jwt');
-            const notLoggedIn = <h3>Please login to see the users</h3>;
-            
-			return (token ? <Component {...this.props} /> : notLoggedIn);
+			const notLoggedIn = <h3>Please login to see the users</h3>;
+
+			return token ? <Component {...this.props} /> : notLoggedIn;
 		}
 	};
 }
